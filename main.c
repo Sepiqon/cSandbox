99f5a8dfd1b279bc0324f543b4bb398527826271
sepiqon <kalinuxhacker@gmail.com>
2023-08-07 19:53:33 +0200

    Start

---
+++ main.c
@@ -0,0 +1,13 @@
+
+//  gcc  -o prog  -LC:\Users\RyZen\Desktop\ch431 CH341WriteI2C.c -lCH341DLL
+#include <windows.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+
+    printf("Siemaaa1\n");
+    printf("Siemaaa2\n");
+    getchar();
+    return 0;
+}
